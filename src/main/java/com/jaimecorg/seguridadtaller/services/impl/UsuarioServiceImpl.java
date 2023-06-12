9e391e5ca41e3dc498115a85fc847364e2e5f945
Jaime Corpas <jaime.corpas.785@estudiante.iesoretania.es>
2023-06-12 11:14:53 +0200

    Corregidos algunos errorres

--- src/main/java/com/jaimecorg/seguridadtaller/services/impl/UsuarioServiceImpl.java
+++ src/main/java/com/jaimecorg/seguridadtaller/services/impl/UsuarioServiceImpl.java
@@ -6,14 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.jaimecorg.seguridadtaller.model.Permiso;
 import com.jaimecorg.seguridadtaller.model.Usuario;
 import com.jaimecorg.seguridadtaller.repository.UsuarioRepository;
+import com.jaimecorg.seguridadtaller.services.PermisoService;
 import com.jaimecorg.seguridadtaller.services.UsuarioService;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService{
     @Autowired
     UsuarioRepository repository;
+
+    @Autowired
+    private PermisoService permisoService;
     
     @Override
     public List<Usuario> findAll() {
@@ -35,10 +41,31 @@ public class UsuarioServiceImpl implements UsuarioService{
         return repository.save(usuario);
     }
 
-    @Override
+    /* @Override
     public void update(int id, Usuario usuario) {
         this.findById(id);
         usuario.setCodigo(id);
+        repository.save(usuario);
+    } */
+
+    @Override
+    public void update(int id, Usuario usuario) {
+        usuario.setCodigo(id);
+
+        List<Permiso> permisos = permisoService.findAll();
+
+
+        for(Permiso permiso :  usuario.getPermisos()) {
+
+
+            int posicion = permisos.indexOf(permiso);
+            Permiso per = permisos.get(posicion);
+
+            permiso.setDescripcion(per.getDescripcion());
+        }
+
+        usuario.setCodigo(id);
+
         repository.save(usuario);
     }
 
